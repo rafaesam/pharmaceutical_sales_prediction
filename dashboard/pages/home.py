d3046150ae7f7bf81b2c0c9c55bbf6ecf92557c6
rafaa <rafaesam0@gmail.com>
2022-05-29 19:21:46 +0300

    commit

---
+++ dashboard/pages/home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+  st.title('Rossmann Pharmaceuticals Sales Prediction')
+  
+  st.write("The finance team at Rossmann Pharmaceuticals wants to forecast sales in all their stores across several cities six weeks ahead of time. Managers in individual stores rely on their years of experience as well as their personal judgment to forecast sales. The data team identified factors such as promotions, competition, school and state holidays, seasonality, and locality as necessary for predicting the sales across the various stores.Your job is to build and serve an end-to-end product that delivers this prediction to analysts in the finance team.")
